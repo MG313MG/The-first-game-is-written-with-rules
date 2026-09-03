e6473da1f1fd24780a51243674791b465f4d0299
Mostafa <mg313mg313mg@gmail.com>
2026-09-03 21:42:47 +0330

    I added two specifications to abilities

--- Assets/DataBase/Scripts/Abilities.cs
+++ Assets/DataBase/Scripts/Abilities.cs
@@ -5,5 +5,6 @@ public class Abilities : ScriptableObject
     public string AbilityName;
     public float StaminaCost;
     public float CoolDownTime;
+    public bool isActivated;
     public float ActivateTime;
 }
