ce8fec76f25d093a833bd5537f8b768b1fdf0319
Mostafa <mg313mg313mg@gmail.com>
2026-09-25 12:06:17 +0330

    Script player stamina controller added

---
+++ Assets/Scripts/Player/PlayerStaminaController.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+public class PlayerStaminaController : MonoBehaviour
+{
+    [Header("Stamina")]
+    [SerializeField]
+    private float _currentStamina;
+    public float MaxStamina;
+
+    public Action<float> StaminaSender;
+
+    private bool isRechargeMode;
+
+    void Start()
+    {
+        if (_currentStamina < MaxStamina)
+            StartCoroutine(_theStaminaRechargerTimer());
+    }
+
+    public void ThePlayerStaminaConsumption(float consumptionValue)
+    {
+        if (_currentStamina >= consumptionValue)
+        {
+            _currentStamina -= consumptionValue;
+            StaminaSender?.Invoke(_currentStamina);
+        }
+        else
+            print("You dont have enough stamina");
+        StopAllCoroutines();
+        isRechargeMode = false;
+        StartCoroutine(_theStaminaRechargerTimer());
+    }
+
+    public void TheStaminaRecharger(float rechargeValue)
+    {
+
+        _currentStamina += rechargeValue;
+        if(_currentStamina > MaxStamina)
+            _currentStamina = MaxStamina;
+        StaminaSender?.Invoke(_currentStamina);
+        if (_currentStamina < MaxStamina)
+        {
+            isRechargeMode = true;
+            StartCoroutine(_theStaminaRechargerTimer());
+        }
+        else if (_currentStamina == MaxStamina)
+            StopAllCoroutines();
+    }
+
+    private IEnumerator _theStaminaRechargerTimer()
+    {
+        if (!isRechargeMode)
+            yield return new WaitForSeconds(5);
+        else
+            yield return new WaitForSeconds(2);
+        TheStaminaRecharger(5);
+    }
+}
